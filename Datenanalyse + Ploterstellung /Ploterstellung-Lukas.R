39845f0e932d32e31c47a6ffe7d7c55f558d6702
lukas-koenig-gh <Lukas.koenig2308@gmail.com>
2026-02-22 14:41:09 +0100

    Finale Plots

--- Datenanalyse + Ploterstellung /Ploterstellung-Lukas.R
+++ Datenanalyse + Ploterstellung /Ploterstellung-Lukas.R
@@ -25,43 +25,20 @@ ggplot(plot.1.data, aes(x = Weight, y = LD, color = dose.class, fill = dose.clas
   
   scale_color_viridis_d(option = "mako", begin = 0.3, end = 0.8) +
   scale_fill_viridis_d(option = "mako", begin = 0.3, end = 0.8) +
+  labs(
+    title ="Verteilung der Dosis abhängig vom Gewicht",
+    subtitle = "Gruppiert nach Konzentration von Vancomycin in mg/L nach 24 Stunden",
+    x = "Gewicht (in kg)",
+    y = "Initialdosis"
+    
+  )
   theme_minimal(base_size = 12) +
   theme(
     legend.position = "top"
   )
 
-#Plot2 - Spagetti Line Graph 
-
 plot.2.data <- data %>%
-  select("ID" = "X", "eGFR24", "eGFR48", "eGFR72", "C24", "C48", "C72") %>%
-  pivot_longer(
-    cols = -ID,
-    names_to = c(".value", "Time"),
-    names_pattern = "([A-Za-z]+)(\\d+)"
-  )
-
-plot.2.avg.data <- plot.2.data %>%
-  group_by(Time) %>%
-  summarise(
-    eGFR = mean(eGFR , na.rm = TRUE),
-    C = mean(C, na.rm = TRUE),
-    ID = "Durchschnitt"
-  )
-
-plot.2.final.data <- plot.2.data %>%
-  filter(ID %in% c(7, 11)) %>%
-  mutate(ID = as.character(ID)) %>%
-  bind_rows(plot.2.avg.data)
-
-ggplot(plot.2.final.data, aes(x = C, y = eGFR, color = ID)) +
-  geom_path(arrow = arrow(length = unit(0.3, "cm"), type = "closed")) +
-  geom_point(size = 3) +
-  geom_hline(yintercept = 20) +
-  scale_color_viridis_d(option = "mako", begin = 0.3, end = 0.8) +
-  theme_minimal()
+  select(starts_with("eGFR")) 
   
-ggplot(plot.2.data, aes(x = eGFR, y = C, group = ID)) +
-  geom_path(alpha = 0.1, color = "grey") +
-  geom_path(data = plot.2.final.data, aes(color = ID))
-
+  
   
